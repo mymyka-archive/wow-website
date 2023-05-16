65ec850eee06ac07cbc8ade1032a33bf6bc208ce
Mykyta Kasianenko Mihailovich <kasianenkomykyta@gmail.com>
2023-05-16 22:00:00 +0300

    Updated Learn Controller

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -1,6 +1,6 @@
 class LearnController < ApplicationController
   def index
-    @word_list = %w[fly blue move book sea]
+    @word_list = %w[fly blue move book sea sun]
     @word_cards = DictionaryModule.get_info_about(@word_list)
   end
 end
