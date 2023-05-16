c9e58878eefb0046b94c6c58496c6bee53387709
Danylo Sydorenko <sydorenko2020ks15@student.karazin.ua>
2023-05-16 23:50:45 +0300

    added BS to sign in page

--- app/controllers/concerns/vocabulary_module.rb
+++ app/controllers/concerns/vocabulary_module.rb
@@ -10,4 +10,24 @@ class VocabularyModule
   def self.add_words_to_user_vocabulary(user_id, word_list)
 
   end
+
+
+  @@url = URI.parse('https://processing-module.onrender.com/api/process')
+
+  def self.get_info_about(word_list)
+    url = @@url
+    request = Net::HTTP::Get.new(url.path)
+    request['Content-Type'] = 'application/json'
+    payload = {
+      "word_list" => word_list
+    }
+    json_payload = payload.to_json
+    request.body = json_payload
+
+    response = Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == 'https') do |http|
+      http.request(request)
+    end
+
+    return JSON.parse(response.body)
+  end
 end
