cd7fa25dbb0d7c416d01f7fee6e39873d4306ef1
Danylo Sydorenko <sydorenko2020ks15@student.karazin.ua>
2023-05-17 00:32:01 +0300

    Vocabulary Module Class start

--- app/controllers/concerns/vocabulary_module.rb
+++ app/controllers/concerns/vocabulary_module.rb
@@ -1,33 +1,24 @@
 # frozen_string_literal: true
 
 class VocabularyModule
-  # @param (int, list[String])
-  def self.get_new_words_for_user(user_id, word_list)
+  @base_url = 'https://vocabulary-module.onrender.com/api/vocabulary'
 
+  def self.get_new_words_for_user(user_id, word_list)
+    raise ArgumentError, 'user_id must be integer' unless user_id.is_a? Integer
+    raise ArgumentError, 'word_list must be array of strings' unless word_list.is_a? Array
+
+    # send request to vocabulary-module
+    SendHttpRequest(@base_url + '/get_new_words_for_user', { user_id: user_id, word_list: word_list })
   end
 
   # @param (int, list[String])
   def self.add_words_to_user_vocabulary(user_id, word_list)
+    raise ArgumentError, 'user_id must be integer' unless user_id.is_a? Integer
+    raise ArgumentError, 'word_list must be array of strings' unless word_list.is_a? Array
 
+    SendHttpRequest(@base_url + '/add_new_words_for_user', { user_id: user_id, word_list: word_list })
   end
 
 
-  @@url = URI.parse('https://processing-module.onrender.com/api/process')
 
-  def self.get_info_about(word_list)
-    url = @@url
-    request = Net::HTTP::Get.new(url.path)
-    request['Content-Type'] = 'application/json'
-    payload = {
-      "word_list" => word_list
-    }
-    json_payload = payload.to_json
-    request.body = json_payload
-
-    response = Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == 'https') do |http|
-      http.request(request)
-    end
-
-    return JSON.parse(response.body)
-  end
 end
